401b6bca09d8fb6fb5c3a93b47d68ca26a39398a
masha <Milla-masha@mail.ru>
2016-08-11 16:54:46 +0300

    add baseVO

---
+++ src/main/java/sjc/app/model/vo/BaseVO.java
@@ -0,0 +1,16 @@
+package sjc.app.model.vo;
+
+public class BaseVO
+{
+    private Long id;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+}
---
+++ src/main/java/sjc/app/model/vo/FriendVO.java
@@ -0,0 +1,16 @@
+package sjc.app.model.vo;
+
+public class FriendVO extends UserSmallVO
+{
+    private Integer isFriend;
+
+    public Integer getIsFriend()
+    {
+        return isFriend;
+    }
+
+    public void setIsFriend(Integer isFriend)
+    {
+        this.isFriend = isFriend;
+    }
+}
