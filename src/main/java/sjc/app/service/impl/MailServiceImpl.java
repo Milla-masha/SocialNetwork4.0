56d65ffc25fff050888197a400e8f138229df5f4
masha <Milla-masha@mail.ru>
2016-08-30 15:19:52 +0300

    mail service

--- src/main/java/sjc/app/service/impl/MailServiceImpl.java
+++ src/main/java/sjc/app/service/impl/MailServiceImpl.java
@@ -1,55 +1,62 @@
 package sjc.app.service.impl;
 
 import org.springframework.stereotype.Controller;
+import sjc.app.constant.Constant;
 import sjc.app.service.MailService;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 /**
  * Created by psycl on 22.08.2016.
  */
 
 @Controller
-public class MailServiceImpl implements MailService{
+public class MailServiceImpl implements MailService
+{
 
 
     @Override
     public void sendPasswordToEmail(String subject, String passwordToSend, String toEmail)
     {
-//
-//        Properties props = new Properties();
-//        props.put("mail.smtp.auth", "true");
-//        props.put("mail.smtp.starttls.enable", "true");
-//        props.put("mail.smtp.host", "smtp.gmail.com");
-//        props.put("mail.smtp.port", "587");
-//
-//
-//        Session session = Session.getInstance(props,
-//                new javax.mail.Authenticator()
-//                {
-//                    protected PasswordAuthentication getPasswordAuthentication()
-//                    {
-//                        return new PasswordAuthentication(LOGIN_EMAIL, PASSWORD_EMAIL);
-//                    }
-//                });
-//
-//
-//        try
-//        {
-//            Message message = new MimeMessage(session);
-//            message.setFrom(new InternetAddress(LOGIN_EMAIL));
-//            message.setRecipients(Message.RecipientType.TO,
-//                    InternetAddress.parse(toEmail));
-//            message.setSubject(subject);
-//            message.setText("Hello, this is your password for SoN: "
-//                    + "\n" + passwordToSend);
-//
-//            Transport.send(message);
-//
-//            System.out.println("Done");
-//        } catch (MessagingException mex)
-//        {
-//            mex.printStackTrace();
-//        }
-//
+
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator()
+                {
+                    protected PasswordAuthentication getPasswordAuthentication()
+                    {
+                        return new PasswordAuthentication(Constant.LOGIN_EMAIL, Constant.PASSWORD_EMAIL);
+                    }
+                });
+
+
+        try
+        {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(Constant.LOGIN_EMAIL));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(toEmail));
+            message.setSubject(subject);
+            message.setText("Hello, this is your password for SoN: "
+                    + "\n" + passwordToSend);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+        } catch (MessagingException mex)
+        {
+            mex.printStackTrace();
+        }
+
     }
 
 }
